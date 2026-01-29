0f55b07771cafcea1482e164f267ff3f5d0c7572
Yanko <yanko54th@gmail.com>
2026-01-29 23:24:46 +0900

    dscription.html・sample.html作成、imgs追加、header.htmlのCSS追加、gitignore追記

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def sample(request):
-    return HttpResponse("")
+    # return HttpResponse("")
+    return render(request, "sample.html")  # 画面表示テスト用にHttpRespomseをコメントアウトしてrenderへ 削除予定
