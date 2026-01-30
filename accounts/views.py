f8e61a674da2c287157c2a60c1c130585d6079f1
Yanko <yanko54th@gmail.com>
2026-01-30 17:35:58 +0900

    サインアップ時説明モーダルhtml作成

--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 def sample(request):
     # return HttpResponse("")
-    return render(request, "discription_space.html")  # 画面表示テスト用にHttpRespomseをコメントアウトしてrenderに変更中（削除予定）
+    return render(request, "discription_teams.html")  # 画面表示テスト用にHttpRespomseをコメントアウトしてrenderに変更中（削除予定）
