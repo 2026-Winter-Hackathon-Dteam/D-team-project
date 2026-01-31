13ab2663eed40cd5d5cb872eefea250a12ed0716
Yanko <yanko54th@gmail.com>
2026-01-31 16:42:46 +0900

    サインアップ画面修正・説明モーダル修正、top.htmlをaccountsへ移動

--- accounts/views.py
+++ accounts/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def sample(request):
-    # return HttpResponse("")
-    return render(request, "components/tutorial_modal.html")  # 画面表示テスト用にHttpRespomseをコメントアウトしてrenderに変更中（削除予定）
+    return HttpResponse("")
