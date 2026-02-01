131bc7fff228f6da57968f93241890be9bf5c814
Yanko <yanko54th@gmail.com>
2026-02-01 21:17:44 +0900

    再プルリクのため作業途中保存 imgs追加、members.html途中

--- accounts/views.py
+++ accounts/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 def sample(request):
     # return HttpResponse("")
-    return render(request, "signup_owner.html")
+    return render(request, "accounts/signup_owner.html")
 # 画面表示テスト用にHttpRespomseをコメントアウトしてrenderに変更中（削除予定）
