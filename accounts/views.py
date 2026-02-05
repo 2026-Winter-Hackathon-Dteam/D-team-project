bde0f76e1d03c0032445e3e316119cf7f3a28aac
Yanko <yanko54th@gmail.com>
2026-02-05 15:22:13 +0900

    members.html JS処理

--- accounts/views.py
+++ accounts/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 def sample(request):
     # return HttpResponse("")
-    return render(request, "accounts/create_members.html")
+    return render(request, "accounts/members.html")
 # 画面表示テスト用にHttpRespomseをコメントアウトしてrenderに変更中（削除予定）
