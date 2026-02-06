4c3a6f0c8aedae3ed2491c0deb10882d518c638d
Yanko <yanko54th@gmail.com>
2026-02-06 23:20:44 +0900

    accounts/views.py 修正

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def sample(request):
-    # return HttpResponse("")
-    return render(request, "accounts/members.html")
-# 画面表示テスト用にHttpRespomseをコメントアウトしてrenderに変更中（削除予定）
+    return HttpResponse("")
