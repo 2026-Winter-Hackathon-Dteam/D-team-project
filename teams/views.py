acee097880f6eb7fee3a0a6d4e503f98c00254a6
Makoto <makoto.nishikawa.5234@gmail.com>
2026-02-09 08:46:57 +0900

    fix: PRレビュー修正箇所対応

--- teams/views.py
+++ teams/views.py
@@ -3,8 +3,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Teams, Team_Users
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required # 追記
+from django.views.decorators.http import require_http_method # 追記
 
 User = get_user_model()
+#@login_required #ログイン機能実装後にコメントアウトを外す # 追記
+@require_http_methods(["GET"]) # 追記
 
 def team_index(request):
     # チームを作られた順に並べて取り出す
@@ -14,13 +18,37 @@ def team_index(request):
     # GETパラメータからteam_idを取得（なければNone）
     selected_team_id = request.GET.get("team_id")
 
+       # ここから追記(team_id未指定時)
+    if selected_team_id == None:
+        selected_team = None
+        team_members = []
+    
+        context = {
+            "teams": teams,
+            "selected_team": selected_team,
+            "team_members": team_members
+        }
+        return render(request, "teams/teams.html", context)
+        
+    # ログインユーザー取得（未ログイン時はログイン画面に遷移、space所属確認の時に必要。）
+    if getattr(request, "user", None) and request.user.is_authenticated:
+        current_user = request.user
+    else:
+        current_user = get_object_or_404(User, pk="11111111-1111-1111-1111-222222222001") #テスト用ユーザー
+        # return redirect('login') #本番用
+
+
     # 選択されたチームの初期値
     selected_team = None
     team_members = []
 
     # 選択されたチームのIDが取得されているならチームメンバー情報を取り出す
     if selected_team_id:
-        selected_team = get_object_or_404(Teams, id=selected_team_id)
+        selected_team = get_object_or_404(
+            Teams, 
+            id=selected_team_id,
+            space_id=current_user.space_id # 追記(5,同一space所属で見れるという権限チェックを追加する)
+            )
         team_members = Team_Users.objects.filter(team=selected_team).select_related("user")
 
     context = {
