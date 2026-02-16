6665923f614f3f91abc935462fe2787ce302ab3a
Kyoko-Sakamoto759 <sakamo.1992@gmail.com>
2026-02-16 13:53:05 +0900

    headerのチーム選択の表示しないようにコメントアウト、チーム選択のコンテキストプロセッサの設定を外す

--- teams/urls.py
+++ teams/urls.py
@@ -5,5 +5,4 @@ app_name = "teams"
 
 urlpatterns = [
     path("", views.team_index, name="team_index"),
-    path('set_current/<uuid:team_id>/', views.set_current_team, name='set_current_team'),
 ]
