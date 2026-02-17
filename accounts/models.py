968f988fdba1207453727c0bd7e62fc9bfe3e18f
hino-27 <hnn.t1217@gmail.com>
2026-02-18 00:29:42 +0900

    オーナーユーザーのサインアップ、ログイン処理

--- accounts/models.py
+++ accounts/models.py
@@ -1,35 +1,120 @@
+import re
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-import uuid
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from django.db import transaction
+from spaces.models import Spaces
 
 
+# ***************************************************************************
 # userを作成するための設定
 class CustomUserManager(BaseUserManager):
-    def create_user(self, employee_id, username, password=None, **extra_fields):
+    # ユーザー作成：どの役割にも共通
+    def create_user(self, space, employee_id, password=None, **extra_fields):
+        if not space:
+            raise ValueError('所属スペースは必須です。')
         if not employee_id:
-            raise ValueError('社員IDは必須です')
-        
-        user = self.model(employee_id=employee_id, username=username, **extra_fields)
+            raise ValueError('employee_idは必須です。')
+
+        # ログインIDの生成
+        login_id = f"{space.code}_{employee_id}".upper()
+
+        if not login_id:
+            raise ValueError('login_idの生成に失敗しました。')
+        if not extra_fields.get("name"):
+            raise ValueError('ユーザー名は必須です。')
+        # 権限をデフォルトに設定
+        extra_fields.setdefault("is_profile_public", False)
+
+        # データの保存
+        user = self.model(login_id=login_id, space=space, employee_id=employee_id, **extra_fields)
+        # ハッシュ化して保存
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
         return user
     
-    def create_superuser(self, employee_id, username, password=None, **extra_fields):
+    # 一般ユーザー作成
+    def create_member(self, space, employee_id, password=None, **extra_fields):
+        if not extra_fields.get("is_admin"):
+            extra_fields.setdefault("is_admin", False)
+        
+        return self.create_user(
+            space = space,
+            employee_id=employee_id,
+            password=password,
+            **extra_fields
+        )
+    
+    # オーナー作成
+    def create_owner(self, space, employee_id, password=None, **extra_fields):
+        extra_fields.setdefault("is_admin", True)
+        
+        return self.create_user(
+            space = space,
+            employee_id = employee_id,
+            password = password,
+            **extra_fields
+        )
+
+    # オーナーとスペースの同時作成
+    @transaction.atomic
+    def create_space_with_owner(self, space_name, space_code, employee_id, password, **extra_fields):
+        space = Spaces.objects.create(
+            name = space_name,
+            code = space_code
+        )
+        owner = self.create_owner(
+            space = space,
+            employee_id = employee_id,
+            password = password,
+            **extra_fields
+        )
+        space.owner_user = owner
+        return space, owner
+
+    #スーパーユーザーの作成
+    def create_superuser(self, login_id, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        return self.create_user(employee_id, username, password, **extra_fields)
+        _, employee_id = login_id.split("_")
+        system_space, _ = Spaces.objects.get_or_create(
+            code = "sys",
+            defaults = {"name":"スーパーユーザー専用スペース"}
+        )
+        return self.create_user(system_space, employee_id, password, **extra_fields)
 
 
+# ***************************************************************************
 # Usersテーブル
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, verbose_name="ユーザーID"
     )
-    employee_id = models.CharField(max_length=15, verbose_name="社員ID")
-    login_id = models.CharField(max_length=18, unique=True)
+    employee_id = models.CharField(
+        max_length=15, 
+        validators=[
+            RegexValidator(
+                regex=r'^[A-Z0-9]{1,15}$',
+                message='英数字1文字から15文字で入力してください'
+            )
+        ],
+        verbose_name="社員ID"
+    )
+    login_id = models.CharField(
+        max_length=19,
+        validators=[
+            RegexValidator(
+                regex=r'^[A-Z0-9]{3}_[A-Z0-9]{1,15}$',
+                message='login_idの形式が不正です'
+            )
+        ],
+        unique=True
+    )
     name = models.CharField(max_length=150, verbose_name="ユーザー名") 
-    space = models.ForeignKey("spaces.Spaces", on_delete=models.CASCADE)
+    space = models.ForeignKey("spaces.Spaces", on_delete=models.CASCADE, related_name="users")
     is_admin = models.BooleanField(default=False, verbose_name="管理者権限設定")
     is_profile_public = models.BooleanField(default=False, verbose_name="プライバシー設定")
     created_at = models.DateTimeField(auto_now_add=True)
@@ -45,9 +130,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     # モデルのDB・表示まわりの設定(特にadmin使用時)
     class Meta:
         constraints = [
+            # 社員IDとスペースIDで一意制約
             models.UniqueConstraint(
-                fields=["employee_id","space"],
-                name="unique_user_space"
+                fields = ["employee_id","space"],
+                name = "unique_user_space"
             )
         ]
         db_table = "Users"
@@ -56,4 +142,18 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     
     # adminやログに出力する文字列の設定
     def __str__(self):
-        return f"{self.employee_id}({self.space.code})"
+        return f"{self.login_id}({self.space.code})"
+    
+    def clean(self):
+        if self.employee_id:
+            value_employee_id = self.employee_id.strip().upper()
+
+            if not re.fullmatch(r"^[A-Z0-9]{1,15}$", value_employee_id):
+                raise ValidationError("社員IDは英数字15文字以内")
+            self.employee_id = value_employee_id
+
+    # 保存時に呼ぶuser.save()をオーバーライド
+    def save(self, *args, **kwargs):
+        # フィールド検証, model.clean(), uniqueチェック
+        self.full_clean()
+        super().save(*args, **kwargs)
---
+++ accounts/views_auth.py
@@ -0,0 +1,141 @@
+import secrets
+import string
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.contrib.auth.views import LoginView
+from django.db import IntegrityError
+from .models import CustomUser
+from .forms import OwnerSignUpForm, OwnerMemberCreateForm ,SpaceCreateForm, LoginForm
+
+
+# ***************************************************************************
+# サインアップ：オーナー
+class OwnerSignupView(View):
+    template_name = "accounts/signup_owner.html"
+
+    def get(self, request):
+        space_form = SpaceCreateForm()
+        user_form = OwnerSignUpForm()
+
+        context = {
+            "space_form":space_form,
+            "user_form":user_form,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        # 回答を受け取る
+        space_form = SpaceCreateForm(request.POST)
+        user_form = OwnerSignUpForm(request.POST)
+        context = {
+            "space_form":space_form,
+            "user_form":user_form,
+        }
+
+        # 入力項目のバリデーションチェック
+        if space_form.is_valid() and user_form.is_valid():
+            try:
+                space, owner = CustomUser.objects.create_space_with_owner(
+                    space_name=space_form.cleaned_data["name"],
+                    space_code=space_form.cleaned_data["code"],
+                    password=user_form.cleaned_data["password1"],
+                    name=user_form.cleaned_data["name"],
+                    employee_id=user_form.cleaned_data["employee_id"],
+                )
+                # 正常登録出来たらログインページにリダイレクト
+                return redirect("accounts:login")
+            
+            # データベースのデータ整合性ルール（制約）に違反したときに発生するエラー
+            except IntegrityError:
+                user_form.add_error(None, "登録処理中にエラーが発生しました")
+        
+        return render(request, self.template_name, context)
+
+
+# ***************************************************************************
+# ↓↓↓↓↓↓↓ 作成中のためレビュー対象外 ↓↓↓↓↓↓↓ 
+# サインアップ：一般ユーザー
+class OwnerMemberCreateView(LoginRequiredMixin, View):
+    # 未ログイン時にログイン画面に遷移
+    login_url = "login"
+    template_name = "accounts/modals/create_member.html"
+
+    def get(self, request):
+        self.space = request.user.space
+        user_form = OwnerMemberCreateForm()
+        created_user = None
+
+        # 新規ユーザー作成後にリダイレクト時に登録ユーザー情報を表示
+        created_user_id = request.session.pop("created_user_id", None)
+        initial_password = request.session.pop("initial_password", None)
+        
+        if created_user_id:
+            created_user = CustomUser.objects.get(login_id=created_user_id)
+        context = {
+            "user_form":user_form,
+            # Noneまたはさっき登録したユーザー情報が含まれる
+            "created_user":created_user,
+            "initial_password":initial_password
+        }
+        return render(request, self.template_name, context)
+    
+
+    def post(self, request):
+        self.space = request.user.space
+        user_form = OwnerMemberCreateForm(request.POST)
+
+        if user_form.is_valid():
+            # 新規作成ユーザー登録後の表示のためパスワードを変数に格納
+            raw_password = generate_password()
+            # 所属スペースの取得
+            space = request.user.space
+            if not space:
+                user_form.add_error(None, "所属スペースが見つかりません")
+                return render(request, self.template_name, {"user_form":user_form})
+            
+            try:
+                member = CustomUser.objects.create_member(
+                    space = space,
+                    employee_id = user_form.cleaned_data["employee_id"],
+                    password=raw_password,
+                    name=user_form.cleaned_data["name"]
+                ) 
+                # 新規作成ユーザー登録後の情報表示のためにセッションを保持（URLで渡さない）
+                request.session["initial_password"] = raw_password
+                request.session["created_user_id"] = member.login_id
+                return redirect("create_member_modal")
+            
+            except IntegrityError:
+                user_form.add_error(None, "登録処理中にエラーが発生しました")
+        
+        return render(request, self.template_name, {"user_form":user_form})
+# ↑↑↑↑↑↑ ここまで作成中のためレビュー対象外  ↑↑↑↑↑↑   
+
+# ***************************************************************************
+# ログイン
+class CustomLoginView(LoginView):
+    form_class = LoginForm
+    template_name = "accounts/login.html"
+
+    # ログイン後の遷移先指定
+    def get_success_url(self):
+        user = self.request.user
+        # 役割によって分離
+        if user.is_admin:
+            return reverse_lazy("accounts:top")
+        else:
+            return reverse_lazy("analysis:personal_analysis")
+
+    def form_invalid(self, form):
+        print(form.errors)
+        return super().form_invalid(form)
+
+
+
+# ***************************************************************************
+# ランダムパスワードの生成
+def generate_password(length=10):
+    chars=string.ascii_letters + string.digits
+    return ''.join(secrets.chice(chars)for _ in range(length))
