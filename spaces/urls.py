558526ffe6b21816b779ffd387842b793e103482
Kyoko-Sakamoto759 <sakamo.1992@gmail.com>
2026-02-19 22:23:45 +0900

    スペース名の変更の実装

--- spaces/urls.py
+++ spaces/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import sample, space_edit_get, space_edit_post
+from .views import sample, space_edit
+from .forms import SpaceEditForm
 
 app_name = "spaces"
 
 urlpatterns = [
     path('sample/', sample),
-    path('edit/', space_edit_get, name='space_edit'),  # GET
-    path('edit/', space_edit_post),  # POST
+    path('edit/', space_edit, name='space_edit'),  # GET
+    #path('edit/', space_edit_post),  # POST
 ]
