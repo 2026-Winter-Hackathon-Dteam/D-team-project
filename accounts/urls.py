45d73e24d85be54e88b3ed8f4896939c0ded657f
Yanko <yanko54th@gmail.com>
2026-02-20 21:00:57 +0900

    urls.pyだけ先行PR内容にするとviewあわずエラーのためモーダルテスト用に戻しました（のちに削除予定）

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,19 +1,10 @@
 from django.urls import path
-from .views_auth import OwnerSignupView, OwnerMemberCreateView, CustomLoginView
-from .views_pages import TopView
-from django.contrib.auth.views import LogoutView
-from . import views_pages
+from . import views 
 
-app_name = "accounts"
+app_name = 'accounts'
 
 urlpatterns = [
-    path('top/',TopView.as_view(), name='top'), 
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('signup_owner/', OwnerSignupView.as_view(), name='signup_owner'),
-    path('create_member/', OwnerMemberCreateView.as_view(), name='create_member'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('members/', views_pages.members , name='members'),
-    path('profile/', views_pages.profile , name='profile'),
-    path('edit_member/', views_pages.edit_member , name='edit_member'),
-    path('change_password/', views_pages.change_password , name='change_password'),
+    path('sample/', views.sample, name='members'),
+    # アカウント作成モーダルテスト用
+    path('create-member/', views.test_create_member, name='create_member'),
 ]
