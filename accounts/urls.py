d03b407cf05a5d7297204bd820eee144e69d8b9f
hino-27 <hnn.t1217@gmail.com>
2026-02-21 22:25:58 +0900

    #25と起こるコンフリクト解消（コンフリクト起きたときにこのファイルを優先）

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
+from . import views_pages
 from .views_auth import OwnerSignupView, OwnerMemberCreateView, CustomLoginView
 from .views_pages import TopView
 from django.contrib.auth.views import LogoutView
-from . import views_pages
+
 
 app_name = "accounts"
 
 urlpatterns = [
+    path('sample/', views_pages.sample, name='members'),
+    # アカウント作成モーダルテスト用
+    path('create-member/', views_pages.test_create_member, name='create_member'),
     path('top/',TopView.as_view(), name='top'), 
     path('login/', CustomLoginView.as_view(), name='login'),
     path('signup_owner/', OwnerSignupView.as_view(), name='signup_owner'),
-    path('create_member/', OwnerMemberCreateView.as_view(), name='create_member'),
+    #path('create_member/', OwnerMemberCreateView.as_view(), name='create_member'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('members/', views_pages.members , name='members'),
     path('profile/', views_pages.profile , name='profile'),
