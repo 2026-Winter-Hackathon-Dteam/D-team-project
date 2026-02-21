5bab89003dd10336e7eb1b31cb1d9c0efa389379
Kyoko-Sakamoto759 <sakamo.1992@gmail.com>
2026-02-21 22:05:29 +0900

    spaces削除APIの実装。プルリクエスト23のaccoutsのmodels.pyの修正を反映したものを入れれば、うまくいくはず。

--- spaces/urls.py
+++ spaces/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import sample, space_edit
+from .views import sample, space_edit, space_delete
 from .forms import SpaceEditForm
 
+app_name = "spaces"
 
 urlpatterns = [
     path('sample/', sample),
     path('edit/', space_edit, name='space_edit'),
+    path('delete/<uuid:space_id>/', space_delete, name='space_delete'),
 ]
