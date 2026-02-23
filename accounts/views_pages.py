4492f34d08ffe2d783ab3edf8c17767a5c9908ed
hino-27 <hnn.t1217@gmail.com>
2026-02-23 16:30:11 +0900

    test用ユーザー作成viewの削除

--- accounts/views_pages.py
+++ accounts/views_pages.py
@@ -20,23 +20,6 @@ class TopView(TemplateView):
 
 
 # ***************************************************************************
-# testコード
-def test_create_member(request):
-    # HTMXからのアクセス（GET）なら、入力フォームを返す
-    if request.method == "GET":
-        return render(request, "accounts/modals/create_member_form.html")
-
-    # 作成ボタンが押された（POST）なら、成功画面を返す
-    if request.method == "POST":
-        context = {
-            'space_code': 'DTM',
-            'employee_id': '12345',
-            'generated_password': 'tm9je!&ne8s1',
-        }
-        return render(request, "accounts/modals/create_member_success.html", context)
-
-
-# ***************************************************************************
 # 一般ユーザー作成
 @login_required
 def create_member(request):
